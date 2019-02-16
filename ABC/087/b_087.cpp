14c4dbe4c2c89e3195211e72d9b7771b17611f9b
DLPMSR-PEACH\Zechs <pntsec_msr@outlook.com>
2019-02-17 01:09:52 +0900

    なんか動かん　なんで

---
+++ ABC/087/b_087.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C, X;
+    int total;
+    int ans = 0;
+    cin >> A >> B >> C >> X;
+    for (int i = 0; i <= A; ++i)
+    {
+        for (int j = 0; j <= B; ++j)
+        {
+            for (int k = 0; j <= C; ++k)
+            {
+                total = 500*i + 100*j + 50*k;
+                if (total == X) ++ans;
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
