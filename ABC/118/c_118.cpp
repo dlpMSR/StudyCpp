b27f076f75b3ee3a67e33b89dd055855fd6138ff
DLPMSR-KURO\Zechs <pntsec_msr@outlook.com>
2019-03-01 00:28:12 +0900

    ユークリッドの互除法

--- ABC/118/c_118.cpp
+++ ABC/118/c_118.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 using namespace std;
 
+
 int gcd(int a, int b) {
     if (b==0) return a;
     else gcd(b, a%b);
@@ -11,11 +12,10 @@ int gcd(int a, int b) {
 
 int main(){
     int N;
-    vector<int> A = {0};
     cin >> N;
+    vector<int> A(N, 0);
     for (int i = 0; i < N; ++i) cin >> A[i];
     int ans = A[0];
-    sort(A.begin(), A.end());
     for (int i = 1;i < A.size(); ++i) {
         ans = gcd(ans, A[i]);
     }
